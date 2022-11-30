7f7d57c0ab13a3888f6cd90da47412f65f850417
katmh <hello@katmh.com>
2022-11-30 16:47:25 -0500

    Drop backend testing GUI in

---
+++ api/public/scripts/freet.js
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+
+/**
+ * Fields is an object mapping the names of the form inputs to the values typed in
+ * e.g. for createUser, fields has properties 'username' and 'password'
+ */
+
+function viewAllFreets(fields) {
+  fetch('/api/freets')
+    .then(showResponse)
+    .catch(showResponse);
+}
+
+function viewFreetsByAuthor(fields) {
+  fetch(`/api/freets?author=${fields.author}`)
+    .then(showResponse)
+    .catch(showResponse);
+}
+
+function createFreet(fields) {
+  fetch('/api/freets', {
+    method: 'POST',
+    body: JSON.stringify(fields),
+    headers: {'Content-Type': 'application/json'}
+  })
+    .then(showResponse)
+    .catch(showResponse);
+}
+
+function deleteFreet(fields) {
+  fetch(`/api/freets/${fields.id}`, {method: 'DELETE'})
+    .then(showResponse)
+    .catch(showResponse);
+}
