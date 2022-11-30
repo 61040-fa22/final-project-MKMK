431bba61a2e05531b3e3154805c7d61575dce4a3
Meera Gregerson <meerag@mit.edu>
2022-11-30 15:35:21 -0500

    Create rating model, handoff collection

---
+++ server/handoff/middleware.ts
@@ -0,0 +1,45 @@
+import type {Request, Response, NextFunction} from 'express';
+import {Types} from 'mongoose';
+import HandoffCollection from './collection';
+
+/**
+ * Checks if a handoff with handoffId in req.params exists
+ */
+const isHandoffExists = async (req: Request, res: Response, next: NextFunction) => {
+  const validFormat = Types.ObjectId.isValid(req.params.handoffId);
+  const handoff = validFormat ? await HandoffCollection.findOne(req.params.handoffId) : '';
+  if (!handoff) {
+    res.status(404).json({
+      error: `Handoff with handoff ID ${req.params.handoffId} does not exist.`
+    });
+    return;
+  }
+
+  next();
+};
+
+/**
+ * Checks if the given dates are valid
+ */
+const isValidBorrowDates = async (req: Request, res: Response, next: NextFunction) => {
+  const {itemId, borrowerId, startDate, endDate} = req.body as {itemId: Types.ObjectId; borrowerId: Types.ObjectId; startDate: Date; endDate: Date};
+  const now = new Date();
+  if (now > startDate || startDate > endDate) {
+    res.status(404).json({
+      error: 'Invalid start or end date.'
+    });
+    return;
+  }
+
+  next();
+};
+
+/**
+ * TODO:
+ * isValidHandoffModifier
+ * Checks if the person accepting is the owner of the item
+ */
+export {
+  isHandoffExists,
+  isValidBorrowDates
+};
---
+++ server/rating/model.ts
@@ -0,0 +1,30 @@
+import type {Types} from 'mongoose';
+import {Schema, model} from 'mongoose';
+
+// Type definition for Rating on the backend
+export type Rating = {
+  _id: Types.ObjectId;
+  handoffId: Types.ObjectId;
+  ownerRating: number;
+  borrowerRating: number;
+};
+
+const RatingSchema = new Schema<Rating>({
+  // The ID of the Handoff being rated
+  handoffId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Handoff'
+  },
+  // The rating for the owner of the item borrowed
+  ownerRating: {
+    type: Number
+  },
+  // The rating for the borrow of the item
+  borrowerRating: {
+    type: Number
+  }
+});
+
+const RatingModel = model<Rating>('Rating', RatingSchema);
+export default RatingModel;
