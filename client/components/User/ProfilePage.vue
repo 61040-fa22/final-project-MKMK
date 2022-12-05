7eaa047b77d15dcdee229548f02dd0ab4f07407d
Meera Gregerson <meerag@mit.edu>
2022-12-04 23:57:29 -0500

    made requests responsive

--- client/components/User/ProfilePage.vue
+++ client/components/User/ProfilePage.vue
@@ -1,12 +1,42 @@
 <template>
   <main>
-    <p>page for profile {{ $route.params.id }}</p>
+    <h2>page for profile {{ $route.params.id }}</h2>
+    <img src="https://via.placeholder.com/150x150">
+
+    <div
+      v-if="(user.username === $store.state.username)"
+    >
+      <h2>My Info</h2>
+      <UserRequests 
+        :user="user"
+        :owner="true"
+      />
+      <UserRequests 
+        :user="user"
+        :owner="false"
+      />
+    </div>
   </main>
 </template>
 
 <script>
+import UserRequests from "@/components/Request/UserRequests.vue";
 export default {
-  name: "ProfilePage"
+  name: "ProfilePage",
+  components: {UserRequests},
+  data (){
+    return{
+      user: Object,
+    }
+  },
+  async mounted () {
+    this.$store.commit('refreshRequests');
+    const r = await fetch(`/api/users/${this.$route.params.id}`);
+    if (r.ok) {
+      const res = await r.json();
+      this.user = res.user;
+    }
+  }
 };
 </script>
 
